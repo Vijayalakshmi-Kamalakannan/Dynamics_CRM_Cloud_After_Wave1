cf78a9d157cedb2c77231d9d766cbe1af0f4b436
FaridhahN <faridhah_ahmed@premierinc.com>
2022-06-07 17:32:51 +0530

    Pipeline test case 5342

---
+++ src/main/java/testcases/Pipeline/TestCase_5342.java
@@ -0,0 +1,45 @@
+package testcases.Pipeline;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import driver.Driver;
+import pages.LoginPage;
+import pages.MemberFormPage;
+import utils.DataInputProvider;
+//TFS_ID_5342-Cloud :Verify if new pipeline is created by filling all the mandatory fields
+
+public class TestCase_5342 {
+
+	  	
+	@Test
+	public void verifyLimitedMemberEditablefields(int iRowNumber, String sDataSheetName) throws Exception, InterruptedException  {
+		
+		//1. Login to CRM as Limited member 
+		new LoginPage()
+			.typeEmail(DataInputProvider.getCellData_ColName(iRowNumber, "email", sDataSheetName))
+			.clickNext()
+	  	    .typePassword(DataInputProvider.getCellData_ColName(iRowNumber, "password", sDataSheetName))  
+	  	    .clicSignin()
+	  	    .clicYesInStaySignedin()
+		
+		//2. Go to Workplace >> Take Any Member Account 
+		.selectAccountsTab()		
+		.searchAccount(DataInputProvider.getCellData_ColName(iRowNumber, "crmNumber", sDataSheetName)) 
+		.selectAccountFromSearchResults()
+
+		//3.Verify  Referred By field access 
+			.verifyEstLocIsEditable(DataInputProvider.getCellData_ColName(iRowNumber, "numEstLoc", sDataSheetName))
+			.verifyReferredByIsEditable(DataInputProvider.getCellData_ColName(iRowNumber, "referredBy", sDataSheetName))						
+			.clickNyTabSave()
+			
+			
+			
+			//Data Reset
+			.removeReferredBy();
+		
+						
+		
+				
+			
+	}
+}
